0c427b11bec200b709926a0e0aabc1d74d19eb90
KevDev0247 <donkevin1980s@gmail.com>
2020-12-04 18:12:51 -0500

    Data access

---
+++ Kevs.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using Kevs.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kevs.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Kev's Pizza", Location = "Hamilton", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 1, Name = "Kev's Taco", Location = "Toronto", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 1, Name = "Kev's Dumplings", Location = "Waterloo", Cuisine = CuisineType.Chinese},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- Kevs/Startup.cs
+++ Kevs/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kevs.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,8 @@ namespace Kevs
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
